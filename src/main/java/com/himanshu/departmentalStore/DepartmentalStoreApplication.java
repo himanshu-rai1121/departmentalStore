e371f07fb54969f12621401a754655a6be8d4b19
GEM-himanshu-kumar <himanshu.kumar@geminisolutions.com>
2024-04-26 14:03:44 +0530

    adding actuator and swagger documentation

--- src/main/java/com/himanshu/departmentalStore/DepartmentalStoreApplication.java
+++ src/main/java/com/himanshu/departmentalStore/DepartmentalStoreApplication.java
@@ -1,9 +1,22 @@
 package com.himanshu.departmentalStore;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Departmental Store Application",
+				description = "Spring boot Project to create Api (CRUD operations) for departmental store.",
+				version = "1.0",
+				contact = @Contact(name = "Himanshu Kumar",
+						email = "himanshu.kumar@geminisolutions.com",
+						url = "https://github.com/GEM-himanshu-kumar/departmentalStore.git")
+		)
+)
 public class DepartmentalStoreApplication {
 
 	public static void main(final String[] args) {
